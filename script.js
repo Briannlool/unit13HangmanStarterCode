16bdfb355f7b5ac5e01801af082288184b560501
Brian Chan <brian.chan080@gmail.com>
2017-04-25 20:57:57 +0000

    addition of js codes to hangman

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ function prepareGame() {
 // defines the secret word
   secretWord = ['J','A','V','A','S','C','R', 'I', 'P', 'T'];
 
+  
 // Step 1 TASK: call the drawWord and drawHangman function here (inside the prepareGame function).   
 drawWord()
 drawHangman()  
@@ -22,33 +23,42 @@ var wrongGuesses = [];
 // })
 
 // This function draws the correct amount of underscores under hangman depending on the length of the word.
-function drawWord() {
-    secretWord.forEach(function(){ 
-        $("#word").append("_"); 
-    });
+   function drawWord() {
+    $("#word").empty();
+    secretWord.forEach(function(letter) {
+        if (correctGuesses.includes(letter)) {
+            $("#word").append(letter); 
+        } else {
+            $("#word").append("_");
+        }
+    }); 
 }
-   
 
 // This function draws the hangman images depending on the number of wrong guesses there are.
 function drawHangman() {
-    
 }
+var wrongImages = ["images/Hangman-1.png", "images/Hangman-2.png", "images/Hangman-3.png", "images/Hangman-4", "images/Hangman-5.png", "images/Hangman-6.png"];
 
 $(document).ready(function() {
-// Step 1 TASK: call the prepare game function and print the secret word to the console. Then run this program.
+    // Step 1 TASK: call the prepare game function and print the secret word to the console. Then run this program.
     prepareGame();
     console.log(secretWord);
-    $("body").keypress(function(eventData){
-       var guess =  eventData.key.toUpperCase(); 
-       
-        if(secretWord.includes(guess)){
-        correctGuesses.push(guess);
+    function onKeyDown(event) {
+        var letter = event.key.toUpperCase();
+        if (secretWord.includes(letter)) {
+            correctGuesses.push(letter);
+            console.log(correctGuesses);
+           drawWord();
         } else {
-        wrongGuesses.push(guess); 
+            wrongGuesses.push(letter);
+            var guessesLength = wrongGuesses.length; 
+            console.log(wrongGuesses);
+            $("#hangman").attr("src", wrongImages[guessesLength - 1]); 
         }
         console.log(correctGuesses);
-        console.log(wrongGuesses); 
-    }); 
-        
-
+        console.log(wrongGuesses);
+    }
+    $("body").keypress(onKeyDown);
+    
 });
+ 
